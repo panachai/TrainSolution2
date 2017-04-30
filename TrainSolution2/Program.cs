45ffaf1549052c63b53fb4340dc0e613c0e57448
Panachai <panachai.ny@gmail.com>
2017-04-30 21:27:01 +0700

    finish จงเขียนโปรแกรม ฝากถอนเงินในธนาคาร โดย ในบัญชีจะต้องมีเงิน อย่างน้อย 100 บาท  สามารถฝาก ถอน และเช็คยอดเงินได้

--- TrainSolution2/Program.cs
+++ TrainSolution2/Program.cs
@@ -10,10 +10,65 @@ namespace TrainSolution2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ssss"); //สรุปการโยนลูกเต๋าให้เท่ากับ 1 ว่ากี่ครั้ง
-            String s = Console.ReadLine();
-            Console.WriteLine(s);
-            Console.ReadLine();
+            Boolean loop = true;
+            int balance = 100;
+
+            while (loop)
+            {
+                Console.WriteLine("\nAccount");
+                Console.WriteLine("=================");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Check Balance");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select number : ");
+
+                int answer = Int32.Parse(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 0:
+                        Console.WriteLine(0);
+                        loop = false;
+                        break;
+                    case 1: //deposit
+                        Console.Write("\nEnter money to deposit : ");
+                        int income = Int32.Parse(Console.ReadLine());
+                        balance += income;
+                        Console.Write("Account balance is : " + balance);
+                        Console.Write("\nPress any key...");
+                        Console.ReadLine();
+                        break;
+                    case 2: //withdraw
+                        Console.Write("\nEnter money to withdraw : ");
+                        int outcome = Int32.Parse(Console.ReadLine());
+
+                        if ((balance - outcome) < 100)
+                        {
+                            Console.Write("Cannot withdraw becourse money will less 100");
+                            Console.Write("\nPress any key...");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            balance -= outcome;
+                            Console.Write("Account balance is : " + balance);
+                            Console.Write("\nPress any key...");
+                            Console.ReadLine();
+                        }
+
+
+                        break;
+                    case 3: //check Balance
+                        Console.WriteLine("\nAccount balance is : " + balance);
+                        Console.Write("Press any key...");
+                        Console.ReadLine();
+                        break;
+                }
+
+            }
+
+
         }
     }
 }
